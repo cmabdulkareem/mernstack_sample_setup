bd111d0caa564c2d8e8a4992217ee7c72e2c4174
cmabdulkareem <cmabdulkareem@gmail.com>
2024-04-03 22:56:14 +0530

    added a middleware to check login

--- routes/user.js
+++ routes/user.js
@@ -4,26 +4,37 @@ const bcrypt = require('bcrypt');
 const UserModel = require('../helpers/user-helper')
 const ProductModel = require('../helpers/product-helper')
 
+
+// Creating a middleware to check to verifyLogin
+const verifyLogin = (req,res,next)=>{
+    if(req.session.loggedIn){
+        next()
+    }else{
+        res.redirect('/login')
+    }
+}
+
 //user route get method
-router.get('/',(req,res)=>{
-    res.render('user/userhome',{admin:false})
+router.get('/', (req, res) => {
+    let user = req.session.user
+    res.render('user/userhome', { admin: false, user })
 })
 
 //signup route get method
-router.get('/signup',(req,res)=>{
-    res.render('user/signup',{admin:false})
+router.get('/signup', (req, res) => {
+    res.render('user/signup', { admin: false })
 })
 
 //signup route post method for user registration
-router.post('/signup',(req,res)=>{
+router.post('/signup', (req, res) => {
     bcrypt.hash(req.body.password, 10)      // hashing body password using bcrypt module
-        .then((hashedpassword)=>{           // Saving hashed password to variable hashedpassword
+        .then((hashedpassword) => {           // Saving hashed password to variable hashedpassword
             req.body.password = hashedpassword; // Updating body password with hashed password
             return UserModel.create(req.body);
         })
-        .then((user)=>{
+        .then((user) => {
             res.redirect(303, 'login')
-        }).catch((error)=>{
+        }).catch((error) => {
             console.log(err)
         })
 })
@@ -40,65 +51,67 @@ router.get('/login', (req, res) => {
 
 
 // login route post method
-router.post('/login',(req,res)=>{
-    const {userName, password} = req.body;
+router.post('/login', (req, res) => {
+    const { userName, password } = req.body;
 
     UserModel.findOne({ userName })
-    .then((user)=>{
-        if(!user){
-            return res.render('user/login',{errorText : "user not found"})
-            // return res.render('user/login',{errorMessage : "User not found"}) //passing error message to login page
-        }
-        bcrypt.compare(password, user.password)
-        .then((result)=>{
-            if(result){
-                req.session.userName = userName;
-                res.redirect('userpage');
-            }else{
-                return res.render('user/login',{errorText : "Invalid password"})
+        .then((user) => {
+            if (!user) {
+                return res.render('user/login', { errorText: "User not found" });
             }
+            bcrypt.compare(password, user.password)
+                .then((result) => {
+                    if (result) {
+                        req.session.loggedIn = true;
+                        req.session.userName = userName; // Set userName in session
+                        req.session.user = user;
+                        res.redirect('/userpage'); // Redirect to '/userpage' upon successful login
+                    } else {
+                        return res.render('user/login', { errorText: "Invalid password" });
+                    }
+                })
+                .catch((error) => {
+                    console.error("Error comparing passwords:", error);
+                    res.status(500).send("Internal server error");
+                });
         })
-        .catch((error)=>{
-            res.status(500).send(error);
+        .catch((error) => {
+            console.error("Error finding user:", error);
+            res.status(500).send("Internal server error");
         });
-    })
-    .catch((error)=>{
-        res.status(500).send(error)
-    });
-})
+});
+
+
 
 // userpage route get method
-router.get('/userpage',(req,res)=>{
-    if (req.session && req.session.userName){
+router.get('/userpage', (req, res) => {
+    if (req.session && req.session.userName) {
         const userName = req.session.userName;
-        res.render('user/userpage', {userName})
-    }else{
+        res.render('user/userpage', { userName })
+    } else {
         res.redirect('login')
     }
 })
 
-// router.get('/',(req,res)=>{
-//     ProductModel.find({}).lean()
-//         .then((products) => {
-//             console.log(products);
-//             res.render('partials/user',{products})
-//         })
-//         .catch((error) => {
-//             res.status(500).send(error);
-//         });
-// });
 
-
-
-router.post('/logout',(req,res)=>{
-    req.session.destroy((err)=>{
-        if(err){
-            console.error("error destroying session", err);
+// Logout get request
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.error("Error destroying session", err);
             res.status(500).send("Internal server error");
-        }else{
-            res.redirect('login')
+        } else {
+            console.log("Session destroyed successfully");
+            res.redirect('/'); // Redirect to the home page
         }
-    })
-})
+    });
+});
+
+
+router.get('/cart', verifyLogin, (req,res)=>{
+    res.render('user/cart');
+});
+
+
 
 module.exports = router;
